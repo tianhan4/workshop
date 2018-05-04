a868abcad80baa222513e324431be2bc4d001d4b
tianhan4 <htianab@qycpu11.cse.ust.hk>
2018-05-04 16:17:15 +0800

    remove cp, tensorflow1.5, adjust the indent format

--- object_detection/eval_util.py
+++ object_detection/eval_util.py
@@ -1,29 +1,3 @@
-# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""Common utility functions for evaluation."""
-import collections
-import logging
-import os
-import time
-
-import numpy as np
-import tensorflow as tf
-
-from object_detection.core import box_list
-from object_detection.core import box_list_ops
-from object_detection.core import keypoint_ops
 from object_detection.core import standard_fields as fields
 from object_detection.metrics import coco_evaluation
 from object_detection.utils import label_map_util
